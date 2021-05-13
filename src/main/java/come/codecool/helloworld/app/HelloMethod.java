2fd042e02776c885b5b5100dc8b09c77d936dc03
bartaodi <barta.kod@gmail.com>
2021-05-13 13:23:58 +0200

    Hello Method

---
+++ src/main/java/come/codecool/helloworld/app/HelloMethod.java
@@ -0,0 +1,10 @@
+package come.codecool.helloworld.app;
+
+public class HelloMethod {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
